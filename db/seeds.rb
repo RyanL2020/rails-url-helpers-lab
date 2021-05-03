14f41abbb8d65e3923f902b1eb100c85ca980c6f
RL2020 <loc.fitness1@gmail.com>
2021-05-03 12:43:01 -0400

    commit tree bdb33e9665a2ef64512bb1e6fef3abd3d0b2a48d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+    1.times do
+        Student.create(first_name: "Sam",
+        last_name: "Jones")
+    end
